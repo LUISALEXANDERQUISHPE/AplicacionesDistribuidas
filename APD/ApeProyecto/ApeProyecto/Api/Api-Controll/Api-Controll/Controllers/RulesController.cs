07a25460f43d5ade2d6f2605d2d47386111792b2
Luis <lquishpe5771@uta.edu.ec>
2025-05-12 20:57:20 -0500

    PrimerParcial

---
+++ APD/ApeProyecto/ApeProyecto/Api/Api-Controll/Api-Controll/Controllers/RulesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ProxyControl.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RulesController : ControllerBase
+    {
+        /*private readonly string badWordsPath = "C:/Users/ASUS/Desktop/sexto/APD/ApeProyecto/squid-proxy-control/bad_words.txt"; //"/etc/squid/bad_words.txt"; 
+        private readonly string blockedSitesPath = "C:/Users/ASUS/Desktop/sexto/APD/ApeProyecto/squid-proxy-control/blocked_sites.txt";//"/etc/squid/bad_words.txt/blocked_sites.txt";
+        */
+        private readonly string badWordsPath = "/config/squid/bad_words.txt";
+        private readonly string blockedSitesPath = "/config/squid/blocked_sites.txt";
+
+
+        [HttpPost("block-word")]
+        public IActionResult BlockWord([FromBody] string word)
+        {
+            System.IO.File.AppendAllText(badWordsPath, $"{word}\n");
+            RestartSquid();
+            return Ok($"Palabra '{word}' bloqueada.");
+        }
+
+        [HttpPost("block-domain")]
+        public IActionResult BlockDomain([FromBody] string domain)
+        {
+            System.IO.File.AppendAllText(blockedSitesPath, $"{domain}\n");
+            RestartSquid();
+            return Ok($"Dominio '{domain}' bloqueado.");
+        }
+
+        private void RestartSquid()
+        {
+            var psi = new ProcessStartInfo
+            {
+                FileName = "bash",
+                Arguments = "-c \"squid -k reconfigure\"",
+                RedirectStandardOutput = true,
+                UseShellExecute = false
+            };
+
+            Process.Start(psi);
+        }
+    }
+}
---
+++ APD/ApeProyecto/ApeProyecto/Api/Api-Controll/Api-Controll/Program.cs
@@ -0,0 +1,30 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// 🔧 Configura Kestrel
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    serverOptions.ListenAnyIP(5000); // 🔥 Escucha todas las IPs en el puerto 5000
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
